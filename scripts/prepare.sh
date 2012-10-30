156cf87ce5e5af02904c158d25694560316ab2c2
Jeff Lindsay <progrium@gmail.com>
2012-10-29 17:48:49 -0700

    need dev version for some gems, and we need to actually use 1.9

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -2,7 +2,10 @@
 apt-get update
 apt-get install build-essential libsqlite3-dev curl rsync git-core \
 libmysqlclient-dev libxml2-dev libxslt-dev libpq-dev libsqlite3-dev \
-genisoimage ruby1.9.1 rubygems ruby-bundler rake debootstrap kpartx -y
+genisoimage ruby1.9.1-dev rubygems ruby-bundler rake debootstrap kpartx -y
+
+update-alternatives --set ruby /usr/bin/ruby1.9.1
+update-alternatives --set gem /usr/bin/gem1.9.1
 
 echo "install: --no-ri --no-rdoc" > /etc/gemrc
 echo "update: --no-ri --no-rdoc" >> /etc/gemrc
