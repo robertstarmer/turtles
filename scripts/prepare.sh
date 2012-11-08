360db1738c0e9d1e76d2eed56b6a4917d0d52492
Jeff Lindsay <progrium@gmail.com>
2012-11-07 18:28:52 -0800

    suddenly a new dependency appears

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,7 +3,7 @@ apt-get update
 apt-get install build-essential libsqlite3-dev curl rsync git-core \
 libmysqlclient-dev libxml2-dev libxslt-dev libpq-dev libsqlite3-dev \
 genisoimage ruby1.9.1-dev rubygems ruby-bundler rake debootstrap \
-kpartx python-setuptools -y
+kpartx python-setuptools qemu -y
 
 update-alternatives --set ruby /usr/bin/ruby1.9.1
 update-alternatives --set gem /usr/bin/gem1.9.1
